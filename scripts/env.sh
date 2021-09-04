1011584bd3b715c6a8a9ddee0219bc92811a54c8
Sam Gammon <sam.g@cookiescalifornia.com>
2021-09-04 01:11:04 -0700

    Mount API key during build

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,3 +5,6 @@ echo "--- :toolbox: Setting up environment...";
 rm -fr .env && virtualenv .env --python python3;
 source .env/bin/activate;
 echo "Virtual env ready.";
+
+echo "--- :key: Acquiring API key...";
+export COOKIES_APIKEY=$(gcloud secrets versions access 1 --secret buildbot_pysdk_apikey);
